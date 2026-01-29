a227e84e0fc2f4a056d0f6305b18b3a46b78674d
Hummner <bence.cservenyak@gmail.com>
2026-01-29 07:37:36 +0100

    add: teamsite, add: bootstrap card, fix: navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MyTeams } from './my-teams/my-teams';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "myteams", component: MyTeams}
+];
